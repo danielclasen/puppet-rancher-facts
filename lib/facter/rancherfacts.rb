bf24d2a472e108df3c2c67ac7741ab74a78b0a64
Daniel Clasen <daniel.clasen@viaboxx.de>
2016-06-06 15:39:59 +0200

    changed fact names to be ruby symbols

--- lib/facter/rancherfacts.rb
+++ lib/facter/rancherfacts.rb
@@ -6,7 +6,7 @@ Facter.add(:docker_executable) do
 end
 
 # determine rancher agent instance id
-Facter.add('rancher_agent_instance_id') do
+Facter.add(:rancher_agent_instance_id) do
   confine :docker_executable => !nil
   setcode do
     Facter::Core::Execution.exec('docker ps --filter="label=io.rancher.container.system=NetworkAgent" -q')
@@ -16,7 +16,7 @@ end
 
 # query rancher-metadata service on the rancher agent docker instance
 # docker exec -it $(docker ps --filter="label=io.rancher.container.system=NetworkAgent" -q) curl  --header 'Accept: application/json' http://localhost/latest/self/host/labels
-Facter.add('rancher_agent_labels') do
+Facter.add(:rancher_agent_labels) do
   confine :docker_executable => !nil
   confine :rancher_agent_instance_id => !nil
   dockerExec = Facter.value(:docker_executable)
