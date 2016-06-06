bec6a8d29d052610b50caf33a81007943f1a14db
Daniel Clasen <daniel.clasen@viaboxx.de>
2016-06-06 12:12:02 +0200

    initial commit

---
+++ lib/facter/rancherfacts.rb
@@ -0,0 +1,28 @@
+# determine docker executable
+Facter.add(:docker_executable) do
+  setcode do
+    Facter::Core::Execution.exec('which docker')
+  end
+end
+
+# determine rancher agent instance id
+Facter.add('rancher_agent_instance_id') do
+  confine :docker_executable => !nil
+  setcode do
+    Facter::Core::Execution.exec('docker ps --filter="label=io.rancher.container.system=NetworkAgent" -q')
+  end
+end
+
+
+# query rancher-metadata service on the rancher agent docker instance
+# docker exec -it $(docker ps --filter="label=io.rancher.container.system=NetworkAgent" -q) curl  --header 'Accept: application/json' http://localhost/latest/self/host/labels
+Facter.add('rancher_agent_labels') do
+  confine :docker_executable => !nil
+  confine :rancher_agent_instance_id => !nil
+  dockerExec = Facter.value(:docker_executable)
+  rancherInstanceId = Facter.value(:rancher_agent_instance_id)
+  setcode do
+    Facter::Core::Execution.exec( dockerExec, 'exec -it',  rancherInstanceId , 'curl  --header \'Accept: application/json\' http://localhost/latest/self/host/labels')
+  end
+end
+
